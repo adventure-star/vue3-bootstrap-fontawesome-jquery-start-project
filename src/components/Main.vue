1682c5921e46e1f6541373e0977bbe41a369f628
adventure-star <Boristolstyakov@protonmail.com>
2021-01-19 08:38:17 +0700

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    Main
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    props: {
+        msg: String
+    },
+    data() {
+
+        return {
+            choice: 'OK',
+        }
+
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+</style>
